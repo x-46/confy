28f5d63cf97906879d359bfe805e30931b59ed64
Marvin Rosenplänter <marvin.rosenplaenter@googlemail.com>
2026-04-23 14:01:58 +0200

    added tests. discovered bug in KmpSearch and fixed it.

--- internal/fileProcessing/multiStringSearch.go
+++ internal/fileProcessing/multiStringSearch.go
@@ -39,16 +39,21 @@ func KmpSearch(text string, words []string) [][]int {
 	nP := make([]int, len(words))
 	occurences := make([][]int, len(words))
 	k := make([]int, len(words))
+	matched := make([]bool, len(words))
 	for j < len(text) {
 		char := text[j]
-		matched := false
+		for i := range matched {
+			matched[i] = false
+		}
+		matchedAny := false
 		for i := range words {
 			if len(words[i]) == 0 || len(words[i]) > len(text) {
 				continue
 			}
-			if words[i][k[i]] == char {
+			matched[i] = words[i][k[i]] == char
+			if matched[i] {
 				k[i] += 1
-				matched = true
+				matchedAny = true
 				if k[i] == len(words[i]) {
 					occurences[i] = append(occurences[i], j+1-k[i])
 					nP[i] += 1
@@ -56,8 +61,16 @@ func KmpSearch(text string, words []string) [][]int {
 				}
 			}
 		}
-		if matched {
+		if matchedAny {
 			j += 1
+			for i := range matched {
+				if !matched[i] {
+					k[i] = T[i][k[i]]
+					if k[i] < 0 {
+						k[i] += 1
+					}
+				}
+			}
 		} else {
 			incJ := false
 			for i := range words {
@@ -95,6 +108,7 @@ func multiReplaceAll(text string, words []string, replacements []string) (string
 	slices.SortFunc(indexWithOccurence, func(a, b [2]int) int {
 		return cmp.Compare(a[1], b[1])
 	})
+	fmt.Println(indexWithOccurence)
 	for i := 0; i < len(indexWithOccurence)-1; i += 1 {
 		if indexWithOccurence[i][1]+len(words[indexWithOccurence[i][0]]) > indexWithOccurence[i+1][1] {
 			return "", fmt.Errorf("unambigious resolution between \"%s\" at %d and \"%s\" at %d", words[indexWithOccurence[i][0]], indexWithOccurence[i][1], words[indexWithOccurence[i+1][0]], indexWithOccurence[i+1][1])
