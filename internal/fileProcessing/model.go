1c407e5b3bdf927fdefcbc6ef6a974f36b0ca10a
Marvin Rosenplänter <marvin.rosenplaenter@googlemail.com>
2026-05-07 20:41:56 +0200

    Parallelization of RecursiveFileProcessing

--- internal/fileProcessing/model.go
+++ internal/fileProcessing/model.go
@@ -6,6 +6,9 @@ type ReplacementPattern struct {
 }
 
 func MultiReplaceAll(fileContent string, patterns []ReplacementPattern) (string, error) {
+	if len(fileContent) == 0 || len(patterns) == 0 {
+		return fileContent, nil
+	}
 	words := make([]string, len(patterns))
 	replacements := make([]string, len(patterns))
 	for i := range patterns {
--- internal/fileProcessing/recursiveFileProcessing.go
+++ internal/fileProcessing/recursiveFileProcessing.go
@@ -1,22 +1,53 @@
 package fileprocessing
 
 import (
+	"errors"
 	"os"
 	"path/filepath"
+	"slices"
 )
 
+func worker(id int, callback func(path string) error, jobs <-chan string, results chan<- error) {
+	for s := range jobs {
+		results <- callback(s)
+	}
+}
+
 func RecursiveFileProcessing(rootDir string, fileExtensions []string, callback func(path string) error) error {
-	return filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
+	jobs := make(chan string, 1)
+	results := make(chan error, 1)
+	defer close(results)
+
+	for i := range 5 { // TODO configurable worker count
+		go worker(i, callback, jobs, results)
+	}
+
+	jbs := []string{}
+	err := filepath.Walk(rootDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 		if !info.IsDir() {
-			for _, ext := range fileExtensions {
-				if filepath.Ext(path) == ext {
-					return callback(path)
-				}
+			if slices.Contains(fileExtensions, filepath.Ext(path)) {
+				jbs = append(jbs, path)
+				return nil
 			}
 		}
 		return nil
 	})
+
+	numJobs := len(jbs)
+	go func() {
+		for _, j := range jbs {
+			jobs <- j
+		}
+		close(jobs)
+	}()
+
+	errs := []error{err}
+	for range numJobs {
+		errs = append(errs, <-results)
+	}
+	<-jobs
+	return errors.Join(errs...)
 }
